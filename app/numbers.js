d8410fd579b492d75661320966342edffec69f30
RoYa <roya3000@ukr.net>
2015-12-05 19:43:57 +0200

    solve numbers

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,21 @@ exports = (typeof window === 'undefined') ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+     return parseInt(num.toString(2)) & (1 << (bit - 1)) ? 1 : 0;
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    if (num < 0 || num > 255 || num % 1 !== 0) {
+        throw new Error(num + " does not fit in a byte");
+    }
+    return ("000000000" + num.toString(2)).substr(-8);
   },
 
   multiply: function(a, b) {
-
+    return parseFloat( (a * b).toPrecision(1) );
   }
 };
